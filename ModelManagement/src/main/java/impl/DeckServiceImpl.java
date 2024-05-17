a8d892c28e265d1c4e9976effde50fce0aeef464
Elsar Chabaev <s0561238@htw-berlin.de>
2024-05-17 17:05:21 +0200

    first commit

---
+++ ModelManagement/src/main/java/impl/DeckServiceImpl.java
@@ -0,0 +1,27 @@
+package impl;
+
+import api.card.api.Card;
+import api.card.api.Deck;
+import api.card.api.iDeck;
+
+public class DeckServiceImpl  implements iDeck {
+    @Override
+    public Deck initialize() {
+        return null;
+    }
+
+    @Override
+    public Deck shuffleDeck(Deck deck) {
+        return null;
+    }
+
+    @Override
+    public boolean isDeckempty(Deck deck) {
+        return false;
+    }
+
+    @Override
+    public void addCardToPile(Card card) {
+
+    }
+}
